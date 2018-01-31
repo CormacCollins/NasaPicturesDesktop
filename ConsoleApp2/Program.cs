ad58ee9fb76d94861cafd66dc3d22200cc190ade
CormacCollins <100655400@swin.student.edu.au>
2018-01-31 16:20:29 +1100

    Sets desktop background correctly, can query image from APOD api

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using static ConsoleApp2.Wallpaper;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string api_key = "DEMO_KEY";
+            string url = String.Format("https://api.nasa.gov/planetary/apod?api_key=" + api_key);
+            Console.WriteLine(url);
+
+            //Bitmap newImage = (Bitmap)Image.FromFile(@"C:\Users\corma\PycharmProjects\untitled\CloudsintheLMC.bmp");
+
+            RestRequest r = new RestRequest();
+            Bitmap newImage = (Bitmap)r.GetRequestImage(url);
+            Style s = Style.Stretched;
+            Wallpaper.Set(newImage, s);
+
+        }
+    }
+}
---
+++ ConsoleApp2/RestRequest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Drawing;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApp2
+{
+
+    class RestRequest
+    {
+        private static HttpWebRequest request;
+        public RestRequest() {
+        }
+
+        /// <summary>
+        /// Returns Stream from url
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private Stream GetReqImage(string url) {
+
+            request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            //Get stream and read JSON response
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            var res = sr.ReadToEnd();
+
+            //Convert result to JSON and get image url 
+            JObject json = JObject.Parse(res);
+            Console.WriteLine(json);
+            string s = json["url"].ToString();
+
+            //Request image for download 
+            request = (HttpWebRequest)WebRequest.Create(s);
+            response = (HttpWebResponse)request.GetResponse();
+            Stream str = response.GetResponseStream();
+            return str;
+        }
+
+        /// <summary>
+        /// Returns image from Get Request from specified url
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public Image GetRequestImage(string url) {
+            Stream str = GetReqImage(url);
+            return Image.FromStream(str);
+        }
+    }
+}
